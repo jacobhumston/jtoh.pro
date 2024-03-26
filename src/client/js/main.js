244b53937d43cc3deec7474bdcb1f7c694ed136b
Jacob Humston's School Account <113031145+jacobhumston-school@users.noreply.github.com>
2024-03-26 13:37:09 +0000

    Got some things about the actual website started.

---
+++ src/client/js/main.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function () {
+    document.title = `${document.title} - jtoh.pro`;
+    document.documentElement.dataset.theme = 'dark';
+});
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import express from 'express';
 import fs from 'node:fs';
 import helmet from 'helmet';
 import { checkOwnedBadgesLarge } from './modules/badgeOwnership.js';
+import getPort from 'get-port';
+import open from 'open';
+import { rateLimit } from 'express-rate-limit';
 
 const config = JSON.parse(fs.readFileSync('config.json'));
 
@@ -11,7 +14,9 @@ const server = express();
 server.disable('x-powered-by');
 server.enable('strict routing');
 server.enable('case sensitive routing');
+server.disable('trust proxy');
 if (config.server.mode === 'production') server.set('env', 'production');
+
 server.use(
     helmet({
         contentSecurityPolicy: {
@@ -23,18 +28,50 @@ server.use(
     })
 );
 
-server.get('/', function (_, response) {
-    response.send('soon :) ~ LovelyJacob - To check your badges, open /api/badge-check/userId');
-});
+server.use(
+    rateLimit({
+        windowMs: 2 * 60 * 1000,
+        limit: 100,
+        standardHeaders: 'draft-7',
+        legacyHeaders: false,
+        validate: { xForwardedForHeader: false }
+    })
+);
+
+server.use(express.static('src/client/', { extensions: 'html' }));
 
 server.get('/api/badge-check/:id', async function (request, response) {
-    const badges = JSON.parse(fs.readFileSync('data/badges.json').toString('utf-8')).badges.map((badge) => badge.id);
-    response.send(await checkOwnedBadgesLarge(request.params.id, badges));
+    if (isNaN(parseInt(request.params.id))) {
+        return response.status(400).send({ error: 'Invalid user id.' });
+    }
+    if (parseInt(request.params.id) <= 0 || parseInt(request.params.id) > 999999999999999) {
+        return response.status(400).send({ error: 'User id out of range.' });
+    }
+    const badges = JSON.parse(fs.readFileSync('data/badges.json').toString('utf-8')).badges;
+    const badgeIds = badges.map((badge) => badge.id);
+    const result = await checkOwnedBadgesLarge(request.params.id, badgeIds);
+    result.forEach((badge) => {
+        const badgeDetails = badges.find((b) => b.id === badge.id);
+        badge.details = {
+            name: badgeDetails.name,
+            description: badgeDetails.description,
+            enabled: badgeDetails.enabled,
+            created: badgeDetails.created,
+            awardedCount: badgeDetails.statistics.awardedCount,
+            winRatePercentage: badgeDetails.statistics.winRatePercentage
+        };
+    });
+    response.send(result);
+});
+
+server.use(function (_, response) {
+    response.status(404).send({ error: '404 - Requested URL not found.' });
 });
 
 if (config.server.mode === 'development') {
-    const port = 80;
+    const port = await getPort({ port: 80 });
     http.createServer(server).listen(port, () => console.log(`Listening on port ${port}.`));
+    open(`http://localhost:${port}`);
 } else if (config.server.mode === 'production') {
     const port = 443;
     https
@@ -46,4 +83,6 @@ if (config.server.mode === 'development') {
             server
         )
         .listen(port, () => console.log(`Listening on port ${port}.`));
+} else {
+    console.log('An invalid server.mode was provided. Please check your configuration file.');
 }
