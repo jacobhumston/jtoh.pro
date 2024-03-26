a7e222627cf1cf15990f951c16cbf53326ad85ae
Jacob Humston <jhumston24@sciototech.org>
2024-03-26 14:07:37 -0400

    Updated main.js

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,2 +1,2 @@
-document.title = `${document.title} - jtoh.pro`;
+document.title = `${document.title} - jtoh.pro (Juke's Towers of Hell Web Utility)`;
 document.documentElement.dataset.theme = 'dark';
