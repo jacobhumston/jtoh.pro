ddd27eb70d9745b5c9f27c76644b013d5aac3f7c
Jacob Humston's School Account <113031145+jacobhumston-school@users.noreply.github.com>
2024-03-27 15:42:48 +0000

    Worked on sizing and such.

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -3,15 +3,5 @@ document.documentElement.dataset.theme = 'dark';
 
 window.addEventListener('load', function () {
     const addAccountButton = document.getElementById('addAccountButton');
-    addAccountButton.addEventListener('click', async function () {
-        const username = prompt('What is the username of the account you wish to add?');
-        if (username) {
-            const response = await (await fetch(`/api/username-to-id/${username}`)).json();
-            if (response.passed === true) {
-                
-            } else {
-                alert("Invalid username provided.")
-            }
-        }
-    });
+    addAccountButton.addEventListener('click', function () {});
 });
