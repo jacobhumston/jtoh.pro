b649d98f13554fef06cacb565cf92561afe995f4
jacobhumston <humstonjacob@gmail.com>
2024-09-08 18:21:22 -0400

    .

--- src/index.ts
+++ src/index.ts
@@ -156,6 +156,7 @@ app.get('/:user', async (context) => {
             ctx.font = '18px Poppins';
             ctx.textAlign = 'left';
             ctx.fillStyle = '#bdbdbd';
+            console.log(towerStats.hardest_raw_difficulty);
             if (
                 towerStats.hardest_abbreviation !== null &&
                 towerStats.hardest_tower !== null &&
