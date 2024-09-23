b622f2056dfc7abd1add35a4c5d32345f13893d9
jacobhumston <lovelyjacob@aol.com>
2024-09-23 16:32:16 -0400

    Update header links in copy.html, faq.html, and update-log.html

--- src/web/app/script.js
+++ src/web/app/script.js
@@ -1,10 +1,12 @@
 function updateExampleOutput() {
     let username = document.getElementById('exampleInputUsername').value;
     if (!username || username.length === 0) username = 'LoveliestJacob';
+    document.getElementById('exampleImageOutput').src = '';
     document.getElementById('exampleImageOutput').src = `/${username}`;
 }
 
 async function copyExampleOutputURL(button) {
+    if (button.innerHTML === `${getGoogleIconHTML('check')} Copied!`) return;
     let username = document.getElementById('exampleInputUsername').value;
     if (!username || username.length === 0) username = 'LoveliestJacob';
     document.getElementById('exampleImageOutput').src = `/${username}`;
@@ -21,24 +23,49 @@ async function copyExampleOutputURL(button) {
         }
     }
     await navigator.clipboard.writeText(`${url}${uuid}`).catch(() => alert('Failed to copy to clipboard!'));
-    let ogText = button.innerText;
-    if (button.innerText === 'Copied!') return;
-    button.innerText = 'Copied!';
+    let ogText = button.innerHTML;
+    button.innerHTML = `${getGoogleIconHTML('check')} Copied!`;
     button.classList.add('successButton');
     setTimeout(() => {
-        button.innerText = ogText;
+        button.innerHTML = ogText;
+        button.classList.remove('successButton');
+    }, 2000);
+}
+
+async function copyExampleOutputImage(button) {
+    if (
+        button.innerHTML === `${getGoogleIconHTML('check')} Copied!` ||
+        button.innerHTML === `${getGoogleIconHTML('downloading')} Loading...`
+    )
+        return;
+    let username = document.getElementById('exampleInputUsername').value;
+    if (!username || username.length === 0) username = 'LoveliestJacob';
+    document.getElementById('exampleImageOutput').src = `/${username}`;
+    const url = document.getElementById('exampleImageOutput').src;
+    let ogText = button.innerHTML;
+    button.innerHTML = `${getGoogleIconHTML('downloading')} Loading...`;
+    const image = await fetch(url)
+        .then((response) => response.blob())
+        .catch(() => null);
+    await navigator.clipboard
+        .write([new ClipboardItem({ 'image/png': image })])
+        .catch(() => alert('Failed to copy to clipboard!'));
+    button.innerHTML = `${getGoogleIconHTML('check')} Copied!`;
+    button.classList.add('successButton');
+    setTimeout(() => {
+        button.innerHTML = ogText;
         button.classList.remove('successButton');
     }, 2000);
 }
 
 async function copyURL(url, button) {
+    if (button.innerHTML === `${getGoogleIconHTML('check')} Copied!`) return;
     await navigator.clipboard.writeText(url).catch(() => alert('Failed to copy to clipboard!'));
-    let ogText = button.innerText;
-    if (button.innerText === 'Copied!') return;
-    button.innerText = 'Copied!';
+    let ogText = button.innerHTML;
+    button.innerHTML = `${getGoogleIconHTML('check')} Copied!`;
     button.classList.add('successButton');
     setTimeout(() => {
-        button.innerText = ogText;
+        button.innerHTML = ogText;
         button.classList.remove('successButton');
     }, 2000);
 }
@@ -70,3 +97,33 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    const root = document.documentElement;
+    const templates = {
+        headerLinks: `<div id="websitesBar">
+                <a href="/app/">JToH</a>
+                <a href="/app/fangame/cscd/">CSCD</a>
+                <a href="/app/fangame/jtohxl/">JToH XL</a>
+                <a href="/app/fangame/eta/">ETA</a>
+                <a href="/app/fangame/atos/">AToS</a>
+            </div>
+            <div id="menuBar">
+                <a href="/app">Home</a>
+                <a href="/app/update-log">Update Log</a>
+                <a href="/app/faq">FAQ</a>
+                <a href="https://discord.com/oauth2/authorize?client_id=1285148080189997107" target="_blank">
+                    Discord Bot
+                </a>
+            </div>`
+    };
+    for (const element of root.getElementsByTagName('template')) {
+        if (element.dataset && templates[element.dataset.name]) {
+            element.outerHTML = templates[element.dataset.name];
+        }
+    }
+});
+
+function getGoogleIconHTML(name) {
+    return `<span class="material-symbols-rounded">${name}</span>`;
+}
