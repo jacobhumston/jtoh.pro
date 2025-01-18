ba349e584660a72f617a92248facf8cfcabe5609
jacobhumston <humstonjacob@gmail.com>
2025-01-18 05:11:18 -0500

    Updated sitemap and other features

--- src/cli/cmd/logs.ts
+++ src/cli/cmd/logs.ts
@@ -10,11 +10,11 @@ export default {
     execute: async () => {
         const convertFileName = (name: string) => name.replaceAll('__', '.').replaceAll('_', ':');
         const sortFiles = (a: string, b: string) =>
-            new Date(convertFileName(a)).getTime() - new Date(convertFileName(b)).getTime();
+            new Date(convertFileName(b)).getTime() - new Date(convertFileName(a)).getTime();
         const current = fs.readdirSync('logs/current/')[0];
-        const files = fs.readdirSync('logs/old/').concat(fs.readdirSync('logs/current/')).sort(sortFiles);
+        const files = fs.readdirSync('logs/old/').concat(fs.readdirSync('logs/current/')).sort(sortFiles).splice(0, 10);
 
-        logger.info(`Found ${files.length} log folders.`);
+        logger.info(`Showing 10 most recent ${files.length} log folders.`);
         if (files.length === 0) return;
 
         const choice = await terminal.singleColumnMenu(
