b127ca74f58765506bb0132e672a10b06f352d2b
LovelyJacob <57332486+jacobhumston@users.noreply.github.com>
2025-08-14 11:51:09 -0400

    Update cache.ts

--- src/cache.ts
+++ src/cache.ts
@@ -4,4 +4,4 @@ import tllCache from '@isaacs/ttlcache';
 export const towerstatsCache = new tllCache({ max: 1000, ttl: convertTo({ minutes: 10 }, 'milliseconds') });
 export const robloxAPICache = new tllCache({ max: 1000, ttl: convertTo({ minutes: 20 }, 'milliseconds') });
 export const leaderboardCache = new tllCache({ max: 1000, ttl: convertTo({ minutes: 10 }, 'milliseconds') });
-export const htmlCache = new tllCache({ max: 9999999999, ttl: 9999999999 });
+export const htmlCache = new tllCache({ max: 2147483647, ttl: 2147483647 });
