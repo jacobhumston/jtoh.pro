4e595818d75ddc2c8b2098bc7f0a62089e2fdbc8
jacobhumston <humstonjacob@gmail.com>
2025-10-02 01:52:27 -0400

    fix: rename cli args to be more simple

--- src/server/config.ts
+++ src/server/config.ts
@@ -8,18 +8,17 @@ import { getCLIArgument, utilCLIValidators } from './managers/argv';
 
 /** Port that the server should run on. Defaults to port 80. */
 export const serverPort: number =
-    (await getCLIArgument('serverPort', 'integer', true, utilCLIValidators.range(80, 6000))) ?? 80;
+    (await getCLIArgument('port', 'integer', true, utilCLIValidators.range(80, 6000))) ?? 80;
 
 /**
  * Whether this server is in development mode or not.
  * Enabling this just adds some useful development tooling.
  * Defaults to true.
  */
-export const isDev: boolean = (await getCLIArgument('isDev', 'boolean', true)) ?? true;
+export const isDev: boolean = (await getCLIArgument('dev', 'boolean', true)) ?? true;
 
 /** URL that can be used to access the website. Defaults to localhost */
-export const serverURL: URL =
-    (await getCLIArgument('serverURL', 'url', true)) ?? new URL(`http://localhost:${serverPort}`);
+export const serverURL: URL = (await getCLIArgument('url', 'url', true)) ?? new URL(`http://localhost:${serverPort}`);
 
 /** Config options as an object. */
 const config = { serverPort, isDev, serverURL };
