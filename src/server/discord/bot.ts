ebe8a8a2d19e6e75daba2c3af30819b8884c1e1b
jacobhumston <humstonjacob@gmail.com>
2025-11-25 01:08:47 -0500

    feat: Got discord bot working

--- src/server/discord/bot.ts
+++ src/server/discord/bot.ts
@@ -5,34 +5,45 @@
  */
 import type { OpenAPIHono } from '@hono/zod-openapi';
 import build from 'dressed/build';
+import { handleRequest, installCommands } from 'dressed/server';
 
 import { rmSync } from 'node:fs';
 
+import { Cache } from '../managers/cache';
+import { log } from '../modules/logger';
 import apiTokens from '../modules/tokens';
-import handleDiscordRequests from './requests';
+
+/** Discord bot cache. */
+export const botCache = new Cache('discord-bot');
 
 /**
  * Boot up the Discord bot.
  * @param app The server's hono app.
  */
 export async function bootDiscordBot(app: OpenAPIHono) {
+    // set env variables
+    process.env.DISCORD_APP_ID = apiTokens.discordInteractionsApplicationId;
+    process.env.DISCORD_PUBLIC_KEY = apiTokens.discordInteractionsPublicKey;
+    process.env.DISCORD_TOKEN = apiTokens.discordInteractionsToken;
+
     // compile commands
     const buildResults = await build({
-        build: { root: 'src/server/discord/' },
-        requests: {
-            env: {
-                DISCORD_APP_ID: apiTokens.discordInteractionsApplicationId,
-                DISCORD_PUBLIC_KEY: apiTokens.discordInteractionsPublicKey,
-                DISCORD_TOKEN: apiTokens.discordInteractionsToken
-            }
-        }
+        build: { root: 'src/server/discord/' }
     });
+
+    // install commands
+    // note that we replace uid to prevent the cache from always updating
+    if (JSON.stringify(buildResults.commands.map((c) => (c.uid = ''))) !== (await botCache.get('commands'))) {
+        await installCommands(buildResults.commands);
+        await botCache.set('commands', JSON.stringify(buildResults.commands.map((c) => (c.uid = ''))));
+        log('success', 'Successfully installed Discord bot commands.');
+    }
 
     // delete '.dressed' directory
     rmSync('.dressed', { force: true, recursive: true });
 
-    // clear console
-    //console.clear();
-
     // handle requests
+    app.post('/api/discord', (context) =>
+        handleRequest(context.req.raw, buildResults.commands, buildResults.components, buildResults.events)
+    );
 }
