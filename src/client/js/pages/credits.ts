ecbdf6ca04755a42ab5f7ce21c87dd2daef02a31
jacobhumston <humstonjacob@gmail.com>
2026-03-31 01:16:28 -0400

    feat: made credits page better

--- src/client/js/pages/credits.ts
+++ src/client/js/pages/credits.ts
@@ -11,7 +11,13 @@ const brandLogos = {
     roblox: 'roblox.jpeg',
     twitch: 'twitch.jpeg',
     x: 'x.jpeg',
-    youtube: 'youtube.jpeg'
+    youtube: 'youtube.jpeg',
+    bluesky: 'bluesky.jpeg',
+    steam: 'steam.jpeg',
+    tiktok: 'tiktok.webp',
+    reddit: 'reddit.jpeg',
+    spotify: 'spotify.jpeg',
+    web: 'web.png'
 };
 
 type brands = keyof typeof brandLogos;
@@ -57,13 +63,23 @@ const credits: Array<{
         name: 'Halo',
         username: 'TheHaloDeveloper',
         role: 'Administrator',
-        bio: 'Creator of TowerStats.com',
+        bio: 'Creator of <a href="https://towerstats.com" target="_blank">TowerStats.com<a>',
         type: 'staff',
         socials: [
+            {
+                brand: 'discord',
+                display: 'thehalodeveloper',
+                url: 'https://discord.com/users/1152788342392508447'
+            },
             {
                 brand: 'roblox',
                 display: 'TheHaloDeveloper',
                 url: 'https://www.roblox.com/users/257770975/profile'
+            },
+            {
+                brand: 'github',
+                display: 'TheHaloDeveloper',
+                url: 'https://github.com/TheHaloDeveloper'
             }
         ]
     },
@@ -71,7 +87,7 @@ const credits: Array<{
         name: 'SirSamiboi',
         username: 'TheRealSamiBoi',
         role: 'Skill Points Creator',
-        bio: null,
+        bio: 'SirSamiboi created the original concept behind Skill Points. It can be found on <a href="https://github.com/SirSamiboi/etoh-pp" target="_blank">GitHub</a>.',
         type: 'contributor',
         socials: [
             {
@@ -104,13 +120,68 @@ const credits: Array<{
         name: 'TehPig',
         username: 'TehPig_YT',
         role: 'Website Moderator',
-        bio: null,
+        bio: '"Half developer, half gamer, fully distracted."',
         type: 'staff',
         socials: [
+            {
+                brand: 'discord',
+                display: 'tehpig_yt',
+                url: 'https://discord.com/users/298432708269441034'
+            },
             {
                 brand: 'roblox',
                 display: 'TehPig_YT',
                 url: 'https://www.roblox.com/users/322468106/profile'
+            },
+            {
+                brand: 'bluesky',
+                display: 'techpig.tehcraft.xyz',
+                url: 'https://bsky.app/profile/did:plc:potoakqb3oazb6k7zdlcgia5'
+            },
+            {
+                brand: 'github',
+                display: 'TehPig',
+                url: 'https://github.com/TehPig'
+            },
+            {
+                brand: 'spotify',
+                display: 'TehPig_YT',
+                url: 'https://open.spotify.com/user/5467mml73tyswxrxxeu3foweo'
+            },
+            {
+                brand: 'steam',
+                display: 'TehPig',
+                url: 'https://steamcommunity.com/id/tehpigyt/'
+            },
+            {
+                brand: 'tiktok',
+                display: 'tehpig_yt',
+                url: 'https://www.tiktok.com/@tehpig_yt'
+            },
+            {
+                brand: 'twitch',
+                display: 'techpiglive',
+                url: 'https://www.twitch.tv/techpiglive'
+            },
+            {
+                brand: 'twitch',
+                display: 'techpigyt',
+                url: 'https://www.twitch.tv/techpigyt'
+            },
+            {
+                brand: 'x',
+                display: 'TechPigYT',
+                url: 'https://x.com/TechPigYT'
+            },
+            {
+                brand: 'youtube',
+                display: 'TechPigYT',
+                url: 'https://www.youtube.com/channel/UC5ir67ugxcQH2_taceOE3Fg'
+            },
+            {
+                brand: 'youtube',
+                display: 'TechPigExtra',
+                url: 'https://www.youtube.com/channel/UCZSeLRViO3nVeZUon2eOscw'
             }
         ]
     },
@@ -177,13 +248,15 @@ window.addEventListener('DOMContentLoaded', async function () {
 
         const bio = document.createElement('p');
         bio.classList.add('bio');
-        bio.innerText = credit.bio ?? '';
+        bio.innerHTML = credit.bio ?? '';
         if (!credit.bio) bio.style.display = 'none';
 
         details.append(role, name, username, bio);
 
         const socials = document.createElement('div');
         socials.classList.add('socials');
+
+        credit.socials = credit.socials.toSorted((a, b) => b.display.localeCompare(a.display));
 
         for (const social of credit.socials) {
             const contact = document.createElement('div');
@@ -197,6 +270,7 @@ window.addEventListener('DOMContentLoaded', async function () {
             const location = document.createElement('a');
             location.classList.add('display');
             location.innerText = social.display;
+            location.target = '_blank';
             if (social.url) location.href = social.url;
 
             contact.append(brand, location);
